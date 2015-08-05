dd3cd936f2050412feb4ca9890aa9b101361c61d
Lance Pioch <lance@bouncefirst.com>
2015-08-05 01:47:41 -0400

    Include variables to preset input values.

--- resources/views/calculator/index.blade.php
+++ resources/views/calculator/index.blade.php
@@ -14,38 +14,40 @@
 	<div class="pure-g">
 	    <div class="pure-u-1"><h1>Q Calculator</h1></div>
     </div>
-    <div class="pure-g">
-    	<div class="pure-u-1"><h2>Addition</h2></div>
-	    <div class="pure-u-1-5"><input type="number" name="augend"></div>
-	    <div class="pure-u-1-5">+</div>
-	    <div class="pure-u-1-5"><input type="number" name="addend"></div>
-	    <div class="pure-u-1-5"><input type="submit" value="="></div>
-	    <div class="pure-u-1-5"><input type="number" id="sum"></div>
-    </div>
-    <div class="pure-g">
-    	<div class="pure-u-1"><h2>Subtraction</h2></div>
-	    <div class="pure-u-1-5"><input type="number" name="minuend"></div>
-	    <div class="pure-u-1-5">–</div>
-	    <div class="pure-u-1-5"><input type="number" name="subtrahend"></div>
-	    <div class="pure-u-1-5"><input type="submit" value="="></div>
-	    <div class="pure-u-1-5"><input type="number" id="difference"></div>
-    </div>
-    <div class="pure-g">
-    	<div class="pure-u-1"><h2>Multiplication</h2></div>
-	    <div class="pure-u-1-5"><input type="number" name="multiplicand"></div>
-	    <div class="pure-u-1-5">x</div>
-	    <div class="pure-u-1-5"><input type="number" name="multiplier"></div>
-	    <div class="pure-u-1-5"><input type="submit" value="="></div>
-	    <div class="pure-u-1-5"><input type="number" id="product"></div>
-    </div>
-    <div class="pure-g">
-    	<div class="pure-u-1"><h2>Division</h2></div>
-	    <div class="pure-u-1-5"><input type="number" name="dividend"></div>
-	    <div class="pure-u-1-5">÷</div>
-	    <div class="pure-u-1-5"><input type="number" name="divisor"></div>
-	    <div class="pure-u-1-5"><input type="submit" value="="></div>
-	    <div class="pure-u-1-5"><input type="number" id="quotient"></div>
-    </div>
+    <form method="post">
+	    <div class="pure-g">
+	    	<div class="pure-u-1"><h2>Addition</h2></div>
+		    <div class="pure-u-1-5"><input type="number" name="augend" value="<?php if (isset($augend)) echo $augend; ?>"></div>
+		    <div class="pure-u-1-5">+</div>
+		    <div class="pure-u-1-5"><input type="number" name="addend" value="<?php if (isset($addend)) echo $addend; ?>"></div>
+		    <div class="pure-u-1-5"><input type="submit" value="="></div>
+		    <div class="pure-u-1-5"><input type="number" id="sum" value="<?php if (isset($sum)) echo $sum; ?>"></div>
+	    </div>
+	    <div class="pure-g">
+	    	<div class="pure-u-1"><h2>Subtraction</h2></div>
+		    <div class="pure-u-1-5"><input type="number" name="minuend" value="<?php if (isset($minuend)) echo $minuend; ?>"></div>
+		    <div class="pure-u-1-5">–</div>
+		    <div class="pure-u-1-5"><input type="number" name="subtrahend" value="<?php if (isset($subtrahend)) echo $subtrahend; ?>"></div>
+		    <div class="pure-u-1-5"><input type="submit" value="="></div>
+		    <div class="pure-u-1-5"><input type="number" id="difference" value="<?php if (isset($difference)) echo $difference; ?>"></div>
+	    </div>
+	    <div class="pure-g">
+	    	<div class="pure-u-1"><h2>Multiplication</h2></div>
+		    <div class="pure-u-1-5"><input type="number" name="multiplicand" value="<?php if (isset($multiplicand)) echo $multiplicand; ?>"></div>
+		    <div class="pure-u-1-5">x</div>
+		    <div class="pure-u-1-5"><input type="number" name="multiplier" value="<?php if (isset($multiplier)) echo $multiplier; ?>"></div>
+		    <div class="pure-u-1-5"><input type="submit" value="="></div>
+		    <div class="pure-u-1-5"><input type="number" id="product" value="<?php if (isset($product)) echo $product; ?>"></div>
+	    </div>
+	    <div class="pure-g">
+	    	<div class="pure-u-1"><h2>Division</h2></div>
+		    <div class="pure-u-1-5"><input type="number" name="dividend" value="<?php if (isset($dividend)) echo $dividend; ?>"></div>
+		    <div class="pure-u-1-5">÷</div>
+		    <div class="pure-u-1-5"><input type="number" name="divisor" value="<?php if (isset($divisor)) echo $divisor; else echo 1; ?>"></div>
+		    <div class="pure-u-1-5"><input type="submit" value="="></div>
+		    <div class="pure-u-1-5"><input type="number" id="quotient" value="<?php if (isset($quotient)) echo $quotient; ?>"></div>
+	    </div>
+    </form>
 </div>
 </body>
 </html>
